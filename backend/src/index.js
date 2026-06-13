c0edee26757925a2092887f2a350b938f73e20a7
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-13 21:53:23 +0530

    add cors and clerk setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,14 +6,17 @@ connectDB();
 
 import express from "express";
 import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use(express.urlencoded({extended: true}));
+app.use(clerkMiddleware())
 
 app.listen(PORT, () => {
     console.log(`Server is listing on http://localhost:${PORT}`)
