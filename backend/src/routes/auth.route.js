c9a20008c8b8868db95d0c69c846959e11522546
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-13 22:46:24 +0530

    add protected auth middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const authRoutes = express.Router();
 
-authRoutes.get("/check", checkAuth);
+authRoutes.get("/check", protectRoute, checkAuth);
 
 export default authRoutes;
