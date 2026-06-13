1b344f4e0868402584ed848109b0835202d2344c
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-13 22:06:24 +0530

    add docker and it's ignore file and also setup fs and path for that

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ dotenv.config();
 import { connectDB } from "./config/db.js";
 connectDB();
 
+import fs from "fs";
+import path from "path";
+
 import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
@@ -13,10 +16,21 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), "public");
+
+
 app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use(express.urlencoded({extended: true}));
 app.use(clerkMiddleware())
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listing on http://localhost:${PORT}`)
