515ff682470ef2b74ad47a0f33c745e7fc046347
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-15 22:58:03 +0530

    add zustand store and react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,25 @@ import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from '@clerk/react';
 import PageLoader from './components/PageLoader';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if(!isLoaded) return <PageLoader />;
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if(!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
     <ThemeProvider>
@@ -18,6 +32,7 @@ const App = () => {
           <Route path='/' element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace/>}/>
           <Route path='/auth' element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace/>}/>
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   )
