7234a8c943055b32a1f40b9a525b641d02505089
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-15 22:05:34 +0530

    add routing and some setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import { Button } from '@heroui/react';
+import { Navigate, Route, Routes } from 'react-router';
+import { ThemeProvider } from './context/ThemeContext';
+import { WallpaperProvider } from './context/WallpaperContext';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
   return (
-    <Button>
-      My Button
-    </Button>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path='/' element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace/>}/>
+          <Route path='/auth' element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace/>}/>
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
